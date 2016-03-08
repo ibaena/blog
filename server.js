ef2f597f896e0960138295168343638c8769d60f
Ivan <ivan.baena04@gmail.com>
2016-03-07 23:15:22 -0500

    set up server and registered three 3 routes

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var expressHandlebars = require('express-handlebars');
+
+
+var PORT = process.env.PORT || 8070;
+
+//SET HANDLEBARS ENGINE
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+//Serve static content for the app from the "public" directory in the application directory.
+app.use('/public', express.static(__dirname + "/public"));
+
+//ROUTES
+app.get('/', function(req, res){
+  res.render('home',{
+    layout:'main',
+    title:'Penniless Developer'
+  });
+});
+
+app.get('/featured', function(req, res){
+  res.render('featured',{
+    layout:'main',
+    title:'Penniless Developer - Featured'
+  });
+});
+
+app.get('/zen', function(req, res){
+  res.render('zen',{
+    layout:'main',
+    title:'Penniless Developer - Zen'
+  });
+});
+
+
+
+app.listen(PORT, function() {
+  console.log("Listening on PORT %s", PORT);
+});
