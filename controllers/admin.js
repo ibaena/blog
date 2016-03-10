14f90701fbc05cc52855cb5cfab6fd4b521f9d39
Ivan <ivan.baena04@gmail.com>
2016-03-10 14:06:09 -0500

    successfully posting to mongo

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,8 +7,8 @@ var posts = require('../models/posts');
 
 //ROUTES
 router.get('/njs-admin', function(req, res) {
-  mongoose.model('posts').find(function(err, test){
-    console.log(test);
+  mongoose.model('posts').find(function(err, test) {
+    //console.log(test);
     res.render('admin', {
       layout: 'admin-layout',
       title: 'Penniless Developer - Dashboard',
@@ -19,27 +19,28 @@ router.get('/njs-admin', function(req, res) {
 });
 
 router.post('/njs-admin', function(req, res) {
-    var body = req.body;
+  var body = req.body;
 
-    newPost = Post({
+  var newPosts = posts({
     author: body.author,
     title: body.title,
     subject: body.subject,
     body: body.body
   });
-    newPost.save(function (err, newPost) {
-  if (err) return console.error(err);
-  console.log('did new');
-});
+
+  newPosts.save(function(err, newPost) {
+    if (err) return console.error(err);
+    console.log(newPost);
 
     res.render('admin', {
       success: 'Successfully Posted!',
       layout: 'admin-layout',
       title: 'Penniless Developer - Dashboard',
       head: 'Dashboard',
-      subhead: 'Whats on your mind?'
+      subhead: 'Whats on your mind?',
+      post: newPost
     });
-
+  });
 });
 
 module.exports = router;
